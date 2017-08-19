fe1d7475e5a144113e6aa8180fe139c5aaf6911f
lyneca <lukemtuthill@gmail.com>
2017-08-19 19:22:30 +1000

    updated CUSP parser

--- example.py
+++ example.py
@@ -1,24 +1,20 @@
-assessments = {
-    "INFO1103": [
-        {
-            "name": "CoffeeBot (Assessment Task I)",  # name
-            "date_due": 1503126316,                   # date due
-            "date_available": 1502126316,             # date released
-            "is_exact": True                          # if it's an exact day or a week
-        },
-        {
-            "name": "MazeGame (Assessment Task II)",
-            "date_due": 1505126316,
-            "date_available": 1504126316,
-            "is_exact": True
-        },
-    ],
-    "INFO2315": [
-        {
-            "name": "Challenge Set I",
-            "date_due": 1505126316,
-            "date_available": None,       # if not available
-            "is_exact": False
-        }
-    ]
-}
+[{'assessment_number': '1',
+  'due_string': 'Multiple Weeks',
+  'is_group': 'No',
+  'name': 'Participation in practical programming tasks',
+  'weight': '10.00'},
+ {'assessment_number': '2',
+  'due_string': 'Multiple Weeks',
+  'is_group': 'No',
+  'name': 'Quiz',
+  'weight': '20.00'},
+ {'assessment_number': '3',
+  'due_string': 'Multiple Weeks',
+  'is_group': 'No',
+  'name': 'Lab Skills',
+  'weight': '10.00'},
+ {'assessment_number': '4',
+  'due_string': 'Exam Period',
+  'is_group': 'No',
+  'name': 'Final Exam',
+  'weight': '60.00'}]
