c1b97f16c1afe0a4e01c826c67460c6e2ccc9697
lyneca <lukemtuthill@gmail.com>
2017-08-19 17:08:59 +1000

    added example assessment dict

---
+++ example.py
@@ -0,0 +1,24 @@
+assessments = {
+    "INFO1103": [
+        {
+            "name": "CoffeeBot (Assessment Task I)",  # name
+            "date_due": 1503126316,                   # date due
+            "date_available": 1502126316,             # date released
+            "is_exact": True                          # if it's an exact day or a week
+        },
+        {
+            "name": "MazeGame (Assessment Task II)",
+            "date_due": 1505126316,
+            "date_available": 1504126316,
+            "is_exact": True
+        },
+    ],
+    "INFO2315": [
+        {
+            "name": "Challenge Set I",
+            "date_due": 1505126316,
+            "date_available": None,       # if not available
+            "is_exact": False
+        }
+    ]
+}
