c552cdaa0425c47c4583501ccc5c292257d74034
lyneca <lukemtuthill@gmail.com>
2017-08-20 01:20:29 +1000

    memes

--- parser/__init__.py
+++ parser/__init__.py
@@ -16,19 +16,10 @@ def get_page(link):
 def cusp(course_code):
     """
     Takes a course code and returns a list containing dictionary objects which represent courses.
+
+    All ye who enter, beware. There are list comprehensions abroad.
     """
-    html = get_page('https://cusp.sydney.edu.au/students/view-unit-page/alpha/' + course_code)
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find(string="Assessment Methods:").parent.next_element.next_element.next_element.next_element.next_element
-    list_ds = [[x.text.strip() for x in x.find_all('td')][:-1] for x in table.find_all('tr')][1:]
-    dict_ds = [{
-        'assessment_number': x[0],
-        'name': x[1],
-        'is_group': x[2],
-        'weight': x[3],
-        'due_string': x[4]
-    } for x in list_ds]
-    return dict_ds 
+    return [{'assessment_number': x[0], 'name': x[1], 'is_group': x[2], 'weight': x[3], 'due_string': x[4]} for x in [[x.text.strip() for x in x.find_all('td')][:-1] for x in BeautifulSoup(get_page('https://cusp.sydney.edu.au/students/view-unit-page/alpha/' + course_code), 'html.parser').find(string="Assessment Methods:").parent.next_element.next_element.next_element.next_element.next_element.find_all('tr')][1:]]
 
 def export(dictionary):
     pass  # should probably do something here
---
+++ parser/test.py
@@ -0,0 +1,18 @@
+d = [
+    {
+        'semester_start': 1543847382,
+        'semester_end': 1543847382,
+        'exam_start': 1543847382,
+        'census_date': 1543847382,
+        'stuvac': 1543847382,
+        'midsem': 1543847382
+    },
+    {
+        'semester_start': 1543847382,
+        'semester_end': 1543847382,
+        'exam_start': 1543847382,
+        'census_date': 1543847382,
+        'stuvac': 1543847382,
+        'midsem': 1543847382
+    },
+]
