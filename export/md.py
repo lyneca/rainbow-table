152ed83b61447dae14e05d2ed317670ccaedd4a5
lyneca <lukemtuthill@gmail.com>
2017-08-21 08:36:56 +1000

    started markdown exporter cleanup

--- export/md.py
+++ export/md.py
@@ -1,40 +1,30 @@
-class mdOutput:
-    def __init__(self, codes):
-        self.courseCodes = codes
+class MDExporter:
+    def __init__(self, assessments):
+        self.assessments = assessments
+        self.lines = []
 
-    def printToMd(self):
-        outputFile = open("templates/output.md", "w")
-        outputFile.write("#Assessments\n")
-        for i in self.courseCodes:
-            assignments = self.getAssignments(i)
-            if assignments == None:
-                return None
-            for j in assignments:
-                if j['due_string'] == 'Exam Period':
-                    j['due_string'] = 'in the Exam Period'
-                elif j['due_string'] == 'Multiple Weeks':
-                    j['due_string'] = 'over multiple weeks'
-                else:
-                    j['due_string'] = 'on '+j['due_string']
-                outputFile.write("-Assessment "+str(j['assessment_number'])+ " is "+j['name']+ " and due " +j['due_string']+" (Worth: "+j['weight'] +"%, Group: " + j['is_group']+")\n")
-        outputFile.close()
+    def export(self):
+        self.add_header('Semester Assessment Timetable')
+        if not os.path.exists("output.csv"): open("output.csv", 'x').close()
+        for code in self.assessment:
+            add_header(code, 2)
+            table = [['#', 'Name', 'Weight', 'Due', 'Group']]
+            for assessment in assessments[code]:
+                table.append([
+                    assessment['assessment_number'],
+                    assessment['name'],
+                    assessment['weight'],
+                    assessment['due_string'],
+                    assessment['is_group']
+                ])
+        with open("output.csv") as output_file:
+            output_file.write('\n'.join(self.lines))
 
+    def add_header(self, text, level=1):
+        self.add_line('#' * level + ' ' + text)
 
-
-
-    def getAssignments(self, code):
-        ''' For when i thought we were saving as txt files
-        f = open("data.txt");
-        text = f.read()
-        f.close()
-        o = open("example.py", "w")
-        o.write("assessment = "+text)
-        o.close()'''
-        from data import data
-        data = data.assessment
-        if code not in data:
-            return None
-        return data[code]
+    def add_line(self, string):
+        self.lines.append(string)
 
 a = mdOutput(["INFO1103"])
 a.printToMd()
