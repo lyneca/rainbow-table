74bb70a6b89f632d0b08e1eba490d2aade24857d
lyneca <lukemtuthill@gmail.com>
2017-08-24 07:38:39 +1000

    wrote a regex implementation (should be faster than BeautifulSoup)

--- parse/__init__.py
+++ parse/__init__.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
+import re
 
 def parse(course_code):
     pass
@@ -11,14 +12,21 @@ def get_page(link):
     """
     r = requests.get(link)
     r.raise_for_status()
-    return r.content
+    return r.content.decode()
 
-def cusp(course_code):
+def cusp_old(course_code):
     """
     Takes a course code and returns a list containing dictionary objects which represent courses.
     All ye who enter, beware. There are list comprehensions abroad.
     """
     return [{'assessment_number': x[0], 'name': x[1], 'is_group': x[2], 'weight': x[3], 'due_string': x[4]} for x in [[x.text.strip() for x in x.find_all('td')][:-1] for x in BeautifulSoup(get_page('https://cusp.sydney.edu.au/students/view-unit-page/alpha/' + course_code), 'html.parser').find(string="Assessment Methods:").parent.next_element.next_element.next_element.next_element.next_element.find_all('tr')][1:]]
+
+def cusp(course_code):
+    html = get_page('https://cusp.sydney.edu.au/students/view-unit-page/alpha/' + course_code)
+    formatted = re.sub(r' {2,}|\n', '', html)
+    # print(re.search(r'Assessment Methods:.+<\/table>', formatted).group())
+    course_list = [re.findall(r'<td>(.+?)</td>', x[0])[:5] for x in re.findall(r'(<tr>(<td>([\s\S]+?)+?</td>)+?<\/tr>)', re.search(r'Assessment Methods:.+<\/table>', formatted).group())]
+    return [{'assessment_number': x[0], 'name': x[1], 'is_group': x[2], 'weight': x[3], 'due_string': x[4]} for x in course_list]
 
 def export(course_code):
     course = cusp(course_code)
